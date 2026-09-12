5d0dcc14f2cdc7068f493544fb0d3e281f87f1af
Yoni-Berihun <yonatanberihun1998@gmail.com>
2026-09-12 16:40:04 +0300

    chore(ui): update Anteneh's role to Technical Product Manager

--- frontend/src/components/MeetTheTeamSheet.jsx
+++ frontend/src/components/MeetTheTeamSheet.jsx
@@ -25,7 +25,7 @@ const TEAM = [
   {
     id: 'anteneh',
     name: 'Anteneh Yimmam',
-    role: 'Product Manager',
+    role: 'Technical Product Manager',
     photo: '/team/anteneh.JPG',
     objectPosition: 'center top',
     linkedin: 'https://www.linkedin.com/in/anteneh-yimmam0/',
